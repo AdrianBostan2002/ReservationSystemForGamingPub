08ebd6570238371a49cd5eed664d125aeb37b53a
Bostan Adrian <bostan.adrian02@gmail.com>
2023-03-23 12:47:30 +0200

    Created first form of Customer class, GamingPub class and Reservation Class

---
+++ ReservationSystemForGamingPub/ReservationSystemForGamingPub/GamingPub.cs
@@ -0,0 +1,23 @@
+﻿namespace ReservationSystemForGamingPub
+{
+    public class GamingPub
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        private int _maxNumberOfClients;
+
+        public int MaxNumberOfClients
+        {
+            get { return _maxNumberOfClients; }
+        }
+
+        public GamingPub(string name, string address, int maxNumberOfClients)
+        {
+            Name = name;
+            Address = address;
+            _maxNumberOfClients = maxNumberOfClients;
+        }
+    }
+}
