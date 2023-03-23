65ee5a287a3ce1bb97208152d2bd1dfa4ffbacb3
Bostan Adrian <bostan.adrian02@gmail.com>
2023-03-23 12:57:24 +0200

    Created DayOfWeek enum and GamingPubScheduleClass

---
+++ ReservationSystemForGamingPub/ReservationSystemForGamingPub/DayOfWeek.cs
@@ -0,0 +1,13 @@
+﻿namespace ReservationSystemForGamingPub
+{
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ ReservationSystemForGamingPub/ReservationSystemForGamingPub/GamingPubSchedule.cs
@@ -0,0 +1,11 @@
+﻿namespace ReservationSystemForGamingPub
+{
+    public class GamingPubSchedule
+    {
+        Dictionary<DayOfWeek, Tuple<int, int>> _schedule;
+
+        public Dictionary<DayOfWeek, Tuple<int, int>> Schedule { get { return _schedule; } }
+
+        public GamingPubSchedule() { }
+    }
+}
